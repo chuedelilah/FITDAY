da29fd8956b5e595fb98da4cfde440bb8f177d34
dean_k501\Dean <mcps5601@gmail.com>
2017-12-25 18:01:58 +0800

    連結藍芽體重計

---
+++ app/src/main/java/beans/fitday/XenonData.java
@@ -0,0 +1,13 @@
+package beans.fitday;
+
+import java.io.Serializable;
+
+/**
+ * Created by Brian on 2016/4/19.
+ */
+public class XenonData implements Serializable {
+    public int productId;
+    public int deviceId;
+    public byte[] values;
+    public int stamp;
+}
