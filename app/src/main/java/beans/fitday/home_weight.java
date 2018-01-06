7f974d144e5ae29936b3e393ff3dd8dabd4354d1
chuedelilah <chuedelilah@gmail.com>
2018-01-06 13:32:59 +0800

    1/6回復

--- app/src/main/java/beans/fitday/home_weight.java
+++ app/src/main/java/beans/fitday/home_weight.java
@@ -10,6 +10,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
@@ -20,100 +22,162 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class home_weight extends AppCompatActivity implements View.OnClickListener  {
+public class home_weight extends AppCompatActivity implements View.OnClickListener ,OnChartValueSelectedListener {
     Button test;
-    private final static String TAG = MainActivity.class.getSimpleName();
-    private Button button4;
-    private BluetoothAdapter mBluetoothAdapter;
-    private int REQUEST_ENABLE_BT = 1;
-    private BluetoothLeService mBluetoothLeService;
-    private xServiceConnection mServiceConnection;
-    private Handler mHandler;
-    private String mBleScanTagId = "";
-
-    private byte[] mPreScanRecord = {0};
-    XenonDecoder mXbDecoder;
-    private boolean mScanning;
-    private String mDeviceAddress;
-    private boolean mEnableScan = true;
-    private static long mBleScanInterval = 20000;//20000
-    private static long mBleScanTimeout = 15000;//15000
-    private EditText editTextWeight;
-
-    class xServiceConnection implements ServiceConnection {
-
-        public xServiceConnection() {
-            super();
-
-        }
-
-        @Override
-        public void onServiceConnected(ComponentName componentName,
-                                       IBinder service) {
-            mBluetoothLeService = ((BluetoothLeService.LocalBinder) service)
-                    .getService();
-            if (!mBluetoothLeService.initialize()) {
-                Log.e(TAG, "Unable to initialize Bluetooth");
-            }
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName componentName) {
-            mBluetoothLeService = null;
-        }
-    }
-
-
+    private LineChart mChart;
+    protected Typeface mTfLight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_weight);
         test=(Button)findViewById(R.id.B);
-        test.setOnClickListener(this);
-        //按鈕事件
-        button4 = (Button) findViewById(R.id.button4);
-        button4.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+         test.setOnClickListener(this);
+        init();
+        setData(20, 30);
 
-                // Check if BLE is supported on the device.
-                if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-                    Toast.makeText(home_weight.this, "BLE is not supported in this device!", Toast.LENGTH_SHORT).show();
-                    //finish();
-                }
+        // redraw
+        mChart.invalidate();
+    }
+    private void init(){
+        mChart = (LineChart) findViewById(R.id.chart1);
+        mChart.setOnChartValueSelectedListener(this);
 
-                // Initializes Bluetooth adapter.
-                final BluetoothManager bluetoothManager =
-                        (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-                mBluetoothAdapter = bluetoothManager.getAdapter();
+        // no description text
+        mChart.getDescription().setEnabled(false);
 
-                // Ensures Bluetooth is available on the device and it is enabled. If not,
-                // displays a dialog requesting user permission to enable Bluetooth.
-                if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-                }
-                mBleScanTagId = XenonHelper.XENON_ADVERTISING_NAME;
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
 
-                // BLE Setting
-                mBluetoothAdapter = bluetoothManager.getAdapter();
+        mChart.setDragDecelerationFrictionCoef(0.9f);
 
-                if (mBluetoothAdapter.isEnabled()) {
-                    mHandler = new Handler();
-                    registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
-                    if (mBleScanInterval != 0) {
-                        mHandler.post(doTask);
-                    }
+        // enable scaling and dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+        mChart.setHighlightPerDragEnabled(true);
 
-                    editTextWeight = (EditText) findViewById(R.id.editText);
-                    scanLeDevice(true);
+        // if disabled, scaling can be done on x- and y-axis separately
+        mChart.setPinchZoom(true);
 
+        // set an alternative background color
+        mChart.setBackgroundColor(Color.LTGRAY);
 
-                }
+        // add data
+        setData(20, 30);
 
-            }
-        });
+        mChart.animateX(
+                2500);
+
+        // get the legend (only possible after setting data)
+        Legend l = mChart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTypeface(mTfLight);
+        l.setTextSize(11f);
+        l.setTextColor(Color.WHITE);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+//        l.setYOffset(11f);
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setTypeface(mTfLight);
+        xAxis.setTextSize(11f);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(false);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setTypeface(mTfLight);
+        leftAxis.setTextColor(ColorTemplate.getHoloBlue());
+        leftAxis.setAxisMaximum(200f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setGranularityEnabled(true);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setTypeface(mTfLight);
+        rightAxis.setTextColor(Color.RED);
+        rightAxis.setAxisMaximum(900);
+        rightAxis.setAxisMinimum(-200);
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setDrawZeroLine(false);
+        rightAxis.setGranularityEnabled(false);
+        rightAxis.setEnabled(false);
+    }
+
+    private void setData(int count, float range) {
+
+        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+            float mult = range / 2f;
+            float val = (float) (Math.random() * mult) + 50;
+            yVals1.add(new Entry(i, val));
+        }
+        LineDataSet set1, set2, set3;
+
+        if (mChart.getData() != null &&
+                mChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
+            set1.setValues(yVals1);
+            mChart.getData().notifyDataChanged();
+            mChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(yVals1, "每日體重紀錄");
+
+            set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+            set1.setColor(ColorTemplate.getHoloBlue());
+            set1.setCircleColor(Color.WHITE);
+            set1.setLineWidth(2f);
+            set1.setCircleRadius(3f);
+            set1.setFillAlpha(65);
+            set1.setFillColor(ColorTemplate.getHoloBlue());
+            set1.setHighLightColor(Color.rgb(244, 117, 117));
+            set1.setDrawCircleHole(false);
+
+            // create a data object with the datasets
+            //LineData data = new LineData(set1, set2, set3);
+            LineData data = new LineData(set1);
+            data.setValueTextColor(Color.WHITE);
+            data.setValueTextSize(9f);
+
+            // set data
+            mChart.setData(data);
+        }
+    }
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        Log.i("Entry selected", e.toString());
+
+        mChart.centerViewToAnimated(e.getX(), e.getY(), mChart.getData().getDataSetByIndex(h.getDataSetIndex())
+                .getAxisDependency(), 500);
+        //mChart.zoomAndCenterAnimated(2.5f, 2.5f, e.getX(), e.getY(), mChart.getData().getDataSetByIndex(dataSetIndex)
+        //mChart.zoomAndCenterAnimated(2.5f, 2.5f, e.getX(), e.getY(), mChart.getData().getDataSetByIndex(dataSetIndex)
+        // .getAxisDependency(), 1000);
+        //mChart.zoomAndCenterAnimated(1.8f, 1.8f, e.getX(), e.getY(), mChart.getData().getDataSetByIndex(dataSetIndex)
+        // .getAxisDependency(), 1000);
+    }
+    @Override
+    public void onNothingSelected() {
+        Log.i("Nothing selected", "Nothing selected.");
     }
     @Override
     public void onClick(View v) {
@@ -125,137 +189,5 @@ public class home_weight extends AppCompatActivity implements View.OnClickListen
         }
     }
 
-    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
 
-        @Override
-        public void onLeScan(final BluetoothDevice device, final int rssi,
-                             final byte[] scanRecord) {
-            mDeviceAddress = device.getAddress();
-            String adName = "";
-            String msg = device.getAddress() + "---payload = ";
-            for (byte b : scanRecord)
-                msg += String.format("%02x ", b);
-            Log.i(TAG, msg);
-            // parse ScanRecord data
-            List<AdRecord> records = AdRecord
-                    .parseScanRecord(scanRecord);
-            XenonData xbData = null;
-            // get advertise Name
-            for (AdRecord packet : records) {
-                Log.i(TAG, "BG Service getType:" + packet.getType());
-                // Find the device name record
-                if (packet.getType() == AdRecord.TYPE_NAME) {
-                    adName = AdRecord.getName(packet);
-                    Log.i(TAG, "BG ADV Name:" + adName);
-                    Log.i(TAG, "BG DEVICE ADDRESS:" + device.getAddress());
-                } else if (packet.getType() == AdRecord.TYPE_MANUFACTURER_SPEC_DATA) {
-                    //  xbData = packet.getXenonData();
-
-                }
-            }
-
-            if (mBluetoothLeService == null) {
-                Intent gattServiceIntent = new Intent(
-                        home_weight.this,
-                        BluetoothLeService.class);
-                mServiceConnection = new xServiceConnection();
-                bindService(gattServiceIntent, mServiceConnection,
-                        BIND_AUTO_CREATE);
-            } else {
-
-                if (!adName.equals("")) {
-                    if (adName.toLowerCase().equals(mBleScanTagId.toLowerCase())) {
-
-                        mBluetoothLeService.connect(mDeviceAddress, false, false, null);
-                    }
-                }
-            }
-            mPreScanRecord = scanRecord;
-
-        }
-    };
-
-
-    private Runnable doTask = new Runnable() {
-        @Override
-        public void run() {
-            try {
-                if (!mScanning) {
-                    if (mEnableScan) {
-                        scanLeDevice(true);
-                    }
-                    //   mHandler.postDelayed(doTask, mBleScanInterval);
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-
-    };
-
-
-    private void scanLeDevice(final boolean enable) {
-        if (enable) {
-            // Stops scanning after a pre-defined scan period.
-            mHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mScanning = false;
-                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
-
-                }
-            }, mBleScanTimeout);
-            mScanning = true;
-
-            mBluetoothAdapter.startLeScan(mLeScanCallback);
-
-        } else {
-            mScanning = false;
-            mBluetoothAdapter.stopLeScan(mLeScanCallback);
-        }
-    }
-
-    private static IntentFilter makeGattUpdateIntentFilter() {
-        final IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED);
-        intentFilter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE);
-        intentFilter.addAction(BluetoothLeService.ACTION_GATT_DISCONNECTED);
-        return intentFilter;
-    }
-
-    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            final String action = intent.getAction();
-            String address;
-            if (BluetoothLeService.ACTION_DATA_AVAILABLE.equals(action)) {
-                try {
-                    Bundle bundle = intent.getBundleExtra(BluetoothLeService.EXTRA_DATA);
-                    address = bundle.getString("address");
-                    byte value[] = bundle.getByteArray("value");
-                    AdRecord adRecord = new AdRecord(value.length + 1, 0xFF, value);
-                    XenonData xbData = adRecord.getXenonData();
-                    String uid = String.format("%02x ", xbData.productId) + String.format("%02x ", xbData.deviceId);
-                    String valueString="";
-                    try {
-                        byte[] weightValue = new byte[5];
-                        weightValue[0] = xbData.values[3];
-                        weightValue[1] = xbData.values[4];
-                        weightValue[2] = xbData.values[5];
-                        weightValue[3] = xbData.values[6];
-                        weightValue[4] = xbData.values[7];
-                        valueString = new String(weightValue, "UTF-8");
-                    }catch(Exception e){
-                        e.printStackTrace();
-                    }
-                    editTextWeight.setText(valueString);
-                    Log.e("values", valueString);
-                    //   mXbDecoder.putData(uid, xbData.values);
-
-                } catch (NullPointerException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    };
 }
